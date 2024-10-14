c31aaca0b53a198cf7e4d02e22c23044bf6ae281
Rafaela Marçal <rafaelamarcal@usp.br>
2024-10-13 21:23:43 -0300

    adicionar exercício 2 P1

---
+++ bash_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Mostrar o diretório atual
+pwd
+
+# Listar detalhes do conteúdo da pasta HOME e redirecionar para o arquivo lista_HOME.txt
+ls -l ~ > lista_HOME.txt
+
+# Mostrar a data atual na tela
+date
